a6a2f8128a84e830116fcbed46f43fd2fc466d72
walde <w-schneider1987@web.de>
2024-02-25 20:36:03 +0900

    added requirement contiguous_iterator because span needs contiguous_iterator. This should improve feedback if someone tries to use createChainViews with std::vector<bool>. vs code support

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,16 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain
 
 
 class Project(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators =  "CMakeDeps"
+
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False #workaround because this leads to useless options in cmake-tools configure
+        tc.generate()
 
     def configure(self):
         self.options["catch2"].with_main = True
--- confu_algorithm/createChainViews.hxx
+++ confu_algorithm/createChainViews.hxx
@@ -3,11 +3,13 @@
 //
 
 #pragma once
+#include <__iterator/concepts.h>
+#include <iterator>
 #include <span>
 #include <vector>
 
-//TODO add option to short cut this function for example for return a chain with length of 3 or more if there is not one with length 3 or more return the longest
-// In this case it would be nice to stop looking for more chains and just return
+// TODO add option to short cut this function for example for return a chain with length of 3 or more if there is not one with length 3 or more return the longest
+//  In this case it would be nice to stop looking for more chains and just return
 /**
  * breaks the chain if pred evaluates to false does contain the breaking element
  * @param cbegin iterator to the begin of a continuous sequence
@@ -21,10 +23,11 @@
  * example output:
  * [[0,0],[1,1,1]]
  */
-namespace confu_algorithm{
+namespace confu_algorithm
+{
 
 auto
-createChainViewsIncludeBreakingElement (auto cbegin, auto cend, auto pred)
+createChainViewsIncludeBreakingElement (std::contiguous_iterator auto cbegin, std::contiguous_iterator auto cend, auto pred)
 {
   if (std::span{ cbegin, cend }.size () == 1)
     {
@@ -64,7 +67,7 @@ createChainViewsIncludeBreakingElement (auto cbegin, auto cend, auto pred)
  * [[0,0],[1,1,1]]
  */
 auto
-createChainViews (auto cbegin, auto cend, auto pred)
+createChainViews (std::contiguous_iterator auto cbegin, std::contiguous_iterator auto cend, auto pred)
 {
   if (std::span{ cbegin, cend }.size () == 1)
     {
